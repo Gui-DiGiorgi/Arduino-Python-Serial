3b0b0b3e181029b2150bb0ed2f7ad7dc0b1b2a74
Guilherme Di Giorgi <42752556+Gui-DiGiorgi@users.noreply.github.com>
2021-08-06 21:43:24 -0300

    Updating the Slider Class
    I added the new Slider Class to the project.

--- Color Slide (Python).py
+++ Color Slide (Python).py
@@ -7,47 +7,74 @@ import time
 import serial
 
 class slider():
-    def __init__(self,x,y,width):
+    def __init__(self, x, y, width, height, size):
+        
+        slider.g_selected = False
+        
+#         Buttom config:
+        self.size = size
     
+#         Slider config
         self.width = width
-        self.height = width
+        self.height = height
+        self.s_x = x
+        self.s_y = y
+        self.color = (200,200,200)
+#         Block config
+        self.selected = False
+        self.clicked = False
+        self.block = width
         self.x = x
         self.y = y
-        self.mouse = 0
-        self.color = (200,200,200)
+        self.block_color = (100,100,100)
+        
+    def click(self,mouse_x,mouse_y,mouse_p):
+        
+        if mouse_p and not slider.g_selected:
+            if self.s_x<=mouse_x<=self.s_x+self.width and self.s_y<=mouse_y<=self.s_y+self.height:
+                self.selected = True
+                slider.g_selected = True
+                
+        elif not mouse_p:
+            self.selected = False
+            slider.g_selected = False
+            
+        if pygtxt.screen_button_initpos("▲", self.s_x, self.s_y - self.size):
+            
+            self.clicked = True
+            self.y -= 1
+            
+        elif pygtxt.screen_button_initpos("▼", self.s_x, self.s_y + self.height):
+            
+            self.clicked = True
+            self.y += 1
+            
+        else:
+            
+            self.clicked = False
+            
+    def move(self, mouse_x, mouse_y):
+        
+        if self.selected:
+        
+            self.y = mouse_y - self.block//2
+            
+        if self.selected or self.clicked:
+
+            if self.y < self.s_y:
+                self.y = self.s_y
+
+            if self.y > self.s_y + self.height - self.block:
+                self.y = self.s_y + self.height - self.block
 
     def draw(self,window):
-        pygame.draw.rect(win,self.color,(self.x,self.y,self.width,self.height))
+        pygame.draw.rect(window,self.color,(self.s_x,self.s_y,self.width,self.height))
+        pygame.draw.rect(window,self.block_color,(self.x,self.y,self.block,self.block))
         
-def pyformat(a):
-
-    a_final = ""
-    
-    for i in a:
-        a_final+=str(i)
-        a_final+=' '
-        
-    a_final = a_final[0:(len(a_final)-1)]
-    
-    return a_final
-    
-def send_bytes(serial_name,string):
-        
-    bu = string
-    
-    byaa = []
-    
-    for i in bu:
-        byaa.append(i.encode('utf-8')) 
-    byaa.append(b'\n')
-    
-    for b in byaa:
-        serial_name.write(b)
-        
-ser = serial.Serial('COM5', 9600)
+ser = serial.Serial('COM3', 9600)
 if not ser.isOpen():
     ser.open()
-
+        
 pygame.init()
 
 win=pygame.display.set_mode((600,600))
@@ -58,7 +85,7 @@ clock = pygame.time.Clock()
 
 clock_time = 30
 
-txt = pyg_text(20,(255,255,255),"comicsansms",Win=win)
+pygtxt = pyg_text(20,(255,255,255),"msmincho",win)
 
 black = (0,0,0)
 
@@ -68,14 +95,14 @@ run = True
 
 sliders = []
 
-init_y = 455
-
 side = 20
+
+init_y = 200
 
 init_sx = 95
 
 for i in range(3):
-    sliders.append(slider(init_sx, init_y, side))
+    sliders.append(slider(init_sx, init_y, 10, 255 + side/2, side))
     init_sx += 200
     
 fake_color = [0,0,0]
@@ -85,6 +112,10 @@ true_color = (0,0,0)
 now = time.time()
 
 interval = 0.1
+
+for i in sliders:
+    
+    i.y = 455
 
 while run:
     
@@ -97,44 +128,22 @@ while run:
     mouse_p = pygame.mouse.get_pressed()
     mouse_xy = pygame.mouse.get_pos()
     
-    
-    win.fill(true_color)
-    
-    init_x = 100
-    
-    for i in range(3):
-        pygame.draw.rect(win,grey,(init_x,200,10,255))
-        init_x += 200
+#     win.fill(true_color)
+    win.fill(black)
         
     for i in sliders:
+        
+        i.click(mouse_xy[0], mouse_xy[1], mouse_p[0])
+        i.move(mouse_xy[0], mouse_xy[1])
         i.draw(win)
         
-    for i in sliders:
-        if mouse_p[0]:
-            if (i.x<=mouse_xy[0]<=i.x+side) and (i.y<=mouse_xy[1]<=i.y+side) and (i.mouse == 0):
-                pygame.time.delay(15)
-                i.mouse = 1
-            else:
-                pygame.time.delay(15)
-                i.mouse = 0
-                
-    for i in sliders:
-        if i.mouse == 1:
-            i.y = mouse_xy[1]-10
-            if i.y>455:
-                i.y = 455
-            elif i.y<200:
-                i.y = 200
-                
     init_x = 100
-    
-    for i in sliders:
-        y_value = 455 - i.y
-        txt.screen_text_centerpos(y_value,init_x,100)
-        init_x += 200
         
-    for i in range(3):
-        fake_color[i] = 455 - sliders[i].y
+    for n,i in enumerate(sliders):
+        y_value = int(455 - i.y)
+        pygtxt.screen_text_centerpos(y_value, init_x, 100)
+        init_x += 200
+        fake_color[n] = y_value
         
     true_color = tuple(fake_color)
     
